0dc130a7794e7f24be679e0480e72022b8a13157
Snorry0815 <kane_htl@gmx.at>
2018-05-13 20:46:18 +0200

    add replication

--- Source/KrazyKars/Vehicle/GoKart.cpp
+++ Source/KrazyKars/Vehicle/GoKart.cpp
@@ -2,7 +2,30 @@
 
 #include "GoKart.h"
 #include "Components/InputComponent.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+#include "UnrealNetwork.h"
 
+namespace
+{
+
+FString GetEnumText(ENetRole role)
+{
+	switch (role) { 
+	case ROLE_None: 
+		return "None";
+	case ROLE_SimulatedProxy: 
+		return "SimulatedProxy";
+	case ROLE_AutonomousProxy: 
+		return "AutonomousProxy";
+	case ROLE_Authority:
+		return "Authority";
+	default: 
+		return "Error"; 
+	}
+}
+
+}
 
 // Sets default values
 AGoKart::AGoKart()
@@ -10,27 +33,79 @@ AGoKart::AGoKart()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	bReplicates = true;
 }
 
 // Called when the game starts or when spawned
 void AGoKart::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (HasAuthority())
+	{
+		NetUpdateFrequency = 1;
+	}
 }
+
 
 // Called every frame
 void AGoKart::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	FVector force = GetActorForwardVector() * maxDrivingForce * throttle;
+	FVector force = CalculateCurrentForce();
 	FVector acceleration = force / mass;
 	velocity += acceleration * DeltaTime;
 
+	ApplyRotation(DeltaTime);
+
+	UpdateLocationFromVelocity(DeltaTime);
+
+	if (HasAuthority())
+	{
+		replicated_Transformation = GetActorTransform();
+	}
+
+	FVector location = GetActorLocation();
+	auto text = FString::Printf(TEXT("%s Position(%f, %f, %f)"), *GetEnumText(Role), location.X, location.Y, location.Z);
+	DrawDebugString(GetWorld(), FVector(0, 0, 100), text, this, FColor::White, DeltaTime);
+}
+
+FVector AGoKart::CalculateCurrentForce() const
+{
+	FVector force = GetActorForwardVector() * maxDrivingForce * throttle;
+	force -= velocity.GetSafeNormal() * velocity.SizeSquared() * dragCoefficient;
+
+	auto* world = GetWorld();
+	float g = -world->GetGravityZ() / 100;
+	force -= velocity.GetSafeNormal() * g * mass * rollingResistanceCoefficient;
+
+	return force;
+}
+
+void AGoKart::OnRep_ReplicatedTransformation()
+{
+	SetActorTransform(replicated_Transformation);
+}
+
+void AGoKart::ApplyRotation(float DeltaTime)
+{
+	float dx = FVector::DotProduct(GetActorForwardVector(), velocity) * DeltaTime;
+	float rotationAngle = dx * steeringThrow / minimumTurningRadius;
+
+	FQuat rotationDelta(GetActorUpVector(), rotationAngle);
+	AddActorWorldRotation(rotationDelta);
+	velocity = rotationDelta.RotateVector(velocity);
+}
+
+void AGoKart::UpdateLocationFromVelocity(float DeltaTime)
+{
 	FVector move = velocity * DeltaTime * 100.0f;
 
-	AddActorWorldOffset(move);
+	FHitResult result;
+	AddActorWorldOffset(move, true, &result);
+	if (result.IsValidBlockingHit())
+		velocity = FVector::ZeroVector;
 }
 
 // Called to bind functionality to input
@@ -38,10 +113,46 @@ void AGoKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 	PlayerInputComponent->BindAxis("MoveForward", this, &AGoKart::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AGoKart::MoveRight);
 }
-
 
 void AGoKart::MoveForward(float value)
 {
 	throttle = value;
+	Server_MoveForward(value);
+}
+
+void AGoKart::MoveRight(float value)
+{
+	steeringThrow = value;
+	Server_MoveRight(value);
+}
+
+void AGoKart::Server_MoveForward_Implementation(float value)
+{
+	throttle = value;
+}
+
+bool AGoKart::Server_MoveForward_Validate(float value)
+{
+	return value <= 1.0f && value >= -1.0f;
+}
+
+void AGoKart::Server_MoveRight_Implementation(float value)
+{
+	steeringThrow = value;
+}
+
+bool AGoKart::Server_MoveRight_Validate(float value)
+{
+	return value <= 1.0f && value >= -1.0f;
+}
+
+void AGoKart::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(AGoKart, replicated_Transformation);
+	DOREPLIFETIME(AGoKart, velocity);
+	DOREPLIFETIME(AGoKart, throttle);
+	DOREPLIFETIME(AGoKart, steeringThrow);
 }
--- Source/KrazyKars/Vehicle/GoKart.h
+++ Source/KrazyKars/Vehicle/GoKart.h
@@ -6,6 +6,37 @@
 #include "GameFramework/Pawn.h"
 #include "GoKart.generated.h"
 
+USTRUCT()
+struct FGoKartMove
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	float throttle;
+
+	UPROPERTY()
+	float steeringThrow;
+
+	UPROPERTY()
+	float deltaTime;
+
+	UPROPERTY()
+	float time;
+};
+
+USTRUCT()
+struct FGoKartState
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FTransform transform;
+	UPROPERTY()
+	FVector velocity;
+	UPROPERTY()
+	FGoKartMove lastMove;
+};
+
 UCLASS()
 class KRAZYKARS_API AGoKart : public APawn
 {
@@ -28,15 +59,46 @@ public:
 
 
 private:
-	UPROPERTY(EditAnywhere)
-	float mass = 1000;
+	void UpdateLocationFromVelocity(float DeltaTime);
 
-	UPROPERTY(EditAnywhere)
-	float maxDrivingForce = 10000;
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_MoveForward(float value);
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_MoveRight(float value);
 
 	void MoveForward(float value);
+	void MoveRight(float value);
 
+	void ApplyRotation(float DeltaTime);
+	FVector CalculateCurrentForce() const;
+
+	UPROPERTY(EditAnywhere)
+	float mass = 1000.0f;
+
+	UPROPERTY(EditAnywhere)
+	float maxDrivingForce = 10000.0f;
+
+	UPROPERTY(EditAnywhere)
+	float minimumTurningRadius = 10.0f;
+
+	UPROPERTY(EditAnywhere)
+	float dragCoefficient = 16.0f;
+
+	UPROPERTY(EditAnywhere)
+	float rollingResistanceCoefficient = 0.015f;
+
+	UPROPERTY(Replicated)
 	FVector velocity;
 
+	UPROPERTY(Replicated)
 	float throttle;
+	UPROPERTY(Replicated)
+	float steeringThrow;
+
+	UPROPERTY(ReplicatedUsing = OnRep_ReplicatedTransformation)
+	FTransform replicated_Transformation;
+
+	UFUNCTION()
+	void OnRep_ReplicatedTransformation();
 };
